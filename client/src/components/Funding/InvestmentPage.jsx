85797251f88389bab09107dfbb9476529c04a1bd
leelaprasad30 <prasadslp30@gmail.com>
2024-03-27 01:25:42 +0530

    changes in postIdea and funding page

--- client/src/components/Funding/InvestmentPage.jsx
+++ client/src/components/Funding/InvestmentPage.jsx
@@ -1,64 +1,64 @@
 import React from 'react';
 
 const InvestmentPage = ({ companyData }) => {
- return (
- <div className="container mx-auto py-8">
- <div className="max-w-3xl mx-auto">
- <div className="grid grid-cols-2 gap-4">
- <h1 className="text-3xl font-bold mb-4">{companyData.companyName}</h1>
- <div>
- <h2 className="text-xl font-semibold mb-2">Founder</h2>
- <p className="text-gray-700">{companyData.founderName}</p>
- <a className="text-gray-500" href={companyData.founderLinks}>Click More for Founders</a>
- </div>
- <div>
- <h2 className="text-xl font-semibold mb-2">Location</h2>
- <p className="text-gray-700">{companyData.location}</p>
- </div>
- </div>
- <p className="text-gray-700 mb-6">{companyData.companyDescription}</p>
- <div className="mt-6">
- <h2 className="text-xl font-semibold mb-2">Sector</h2>
- <p className="text-gray-700">{companyData.sector}</p>
- </div>
- <div className="mt-6">
- <h2 className="text-xl font-semibold mb-2">Company website</h2>
- <p className="text-gray-700">{companyData.otherLinks}</p>
- </div>
- <div className="mt-6">
- <h2 className="text-xl font-semibold mb-2">Social Media Links</h2>
- <a href={companyData.socialMediaLinks} target='_blank' className="text-gray-700">Company social links</a>
- </div>
- <div className="mt-6">
- <h2 className="text-xl font-semibold mb-2">Shares Dilute</h2>
- <p className="text-gray-700">{companyData.sharesDilute}</p>
- </div>
- <div className="mt-6">
- <h2 className="text-xl font-semibold mb-2">Funds needed</h2>
- <p className="text-gray-700">{companyData.fundsNeeded}</p>
- </div>
- <div className="mt-6">
- <button className="px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"> Invest in {companyData.companyName} </button>
- </div>
- </div>
- </div>
- );
+    return (
+        <div className="container mx-auto py-8">
+            <div className="max-w-3xl mx-auto">
+                <div className="grid grid-cols-2 gap-4">
+                    <h1 className="text-3xl font-bold mb-4">{companyData.companyName}</h1>
+                    <div>
+                        <h2 className="text-xl font-semibold mb-2">Founder</h2>
+                        <p className="text-gray-700">{companyData.founderName}</p>
+                        <a className="text-gray-500" href={companyData.founderLinks}>Click More for Founders</a>
+                    </div>
+                    <div>
+                        <h2 className="text-xl font-semibold mb-2">Location</h2>
+                        <p className="text-gray-700">{companyData.location}</p>
+                    </div>
+                </div>
+                <p className="text-gray-700 mb-6">{companyData.companyDescription}</p>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold mb-2">Sector</h2>
+                    <p className="text-gray-700">{companyData.sector}</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold mb-2">Company website</h2>
+                    <p className="text-gray-700">{companyData.otherLinks}</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold mb-2">Social Media Links</h2>
+                    <a href={companyData.socialMediaLinks} target='_blank' className="text-gray-700">Company social links</a>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold mb-2">Shares Dilute</h2>
+                    <p className="text-gray-700">{companyData.sharesDilute}</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold mb-2">Funds needed</h2>
+                    <p className="text-gray-700">{companyData.fundsNeeded}</p>
+                </div>
+                <div className="mt-6">
+                    <button className="px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"> Invest in {companyData.companyName} </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 const CompanyInvestPage = () => {
- const appleCompanyData = {
- companyName: 'Apple Inc.',
- founderName: 'Steve Jobs',
- founderLinks: 'https://en.wikipedia.org/wiki/History_of_Apple_Inc.',
- companyDescription: '...',
- location: 'Cupertino, California, United States',
- sector: 'Technology',
- socialMediaLinks: 'https://www.linkedin.com/company/apple/',
- otherLinks: 'https://www.apple.com/',
- sharesDilute: '0%',
- fundsNeeded: '1000000'
- };
- return <InvestmentPage companyData={appleCompanyData} />;
+    const appleCompanyData = {
+        companyName: 'Apple Inc.',
+        founderName: 'Steve Jobs',
+        founderLinks: 'https://en.wikipedia.org/wiki/History_of_Apple_Inc.',
+        companyDescription: '...',
+        location: 'Cupertino, California, United States',
+        sector: 'Technology',
+        socialMediaLinks: 'https://www.linkedin.com/company/apple/',
+        otherLinks: 'https://www.apple.com/',
+        sharesDilute: '0%',
+        fundsNeeded: '1000000'
+    };
+    return <InvestmentPage companyData={appleCompanyData} />;
 };
 
 export default CompanyInvestPage;
--- client/src/components/Ideas/PostIdea.jsx
+++ client/src/components/Ideas/PostIdea.jsx
@@ -3,57 +3,60 @@ import { BiArrowBack } from 'react-icons/bi';
 
 const IdeaPostingPage = () => {
     return (
-        <div className="PostIdea_container">
-            <div className="PostIdea_header">
-                <div className="back_button">
+        <div className="bg-white-200 my-6">
+            <div className="flex justify-between mx-32">
+                <div className="w-10 h-10 cursor-pointer">
                     <BiArrowBack />
                 </div>
-                <div className="PostIdea-profile">
+                <div className="w-10 h-10 rounded-full overflow-hidden">
                     <img src="https://source.unsplash.com/400x400/?man" alt="profile" />
                 </div>
             </div>
-            <p className='text-bold mb-10 text-2xl text-center'>post your idea here</p>
-            <p className='star_text'>(*Drag vertically to increase text area.)</p>
-            <div className="form_section">
-                <div className="form_group">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" />
+            <div className='mt-2 w-8/12 mx-auto bg-white pl-10 pr-24 rounded-lg shadow-md'>
+                <p className='text-bold mb-10 text-2xl text-center'>Post Your Idea Here</p>
+                <p className='text-xs'>(*Drag vertically to increase text area.)</p>
+                <div className="my-4">
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="title" className='block text-md font-medium text-gray-900'>Title</label>
+                        <input type="text" id="title" name="title" className="mt-2 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm" />
+                    </div>
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="problemStatement" className='block text-md font-medium text-gray-900'>Problem Statement</label>
+                        <textarea id="problemStatement" name="problemStatement" className="mt-3 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"></textarea>
+                    </div>
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="approach" className='block text-md font-medium text-gray-900'>Approach</label>
+                        <textarea id="approach" name="approach" className="mt-3 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"></textarea>
+                    </div>
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="uniqueIdea" className='block text-md font-medium text-gray-900'>Unique Idea</label>
+                        <textarea id="uniqueIdea" name="uniqueIdea" className="mt-3 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"></textarea>
+                    </div>
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="description" className='block text-md font-medium text-gray-900'>Description</label>
+                        <textarea id="description" name="description" className="mt-3 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"></textarea>
+                    </div>
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="impact" className='block text-md font-medium text-gray-900'>Impact</label>
+                        <textarea id="impact" name="impact" className="mt-3 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"></textarea>
+                    </div>
+
+                    <div className="mt-3 mb-6">
+                        <label htmlFor="prototype" className='block text-md font-medium text-gray-900'>Prototype</label>
+                        <textarea id="prototype" name="prototype" className="mt-3 p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"></textarea>
+                    </div>
+
                 </div>
 
-                <div className="form_group">
-                    <label htmlFor="problemStatement">Problem Statement</label>
-                    <textarea id="problemStatement" name="problemStatement"></textarea>
+                <div className=" text-center px-4 py-2 rounded-md shadow-sm text-sm font-medium ">
+                    <button type="submit" className='w-32 h-10 font-bold mb-4 text-white bg-orange-500 hover:bg-orange-700 rounded-lg'>Submit Idea</button>
                 </div>
-
-                <div className="form_group">
-                    <label htmlFor="approach">Approach</label>
-                    <textarea id="approach" name="approach"></textarea>
-                </div>
-
-                <div className="form_group">
-                    <label htmlFor="uniqueIdea">Unique Idea</label>
-                    <textarea id="uniqueIdea" name="uniqueIdea"></textarea>
-                </div>
-
-                <div className="form_group">
-                    <label htmlFor="description">Description</label>
-                    <textarea id="description" name="description"></textarea>
-                </div>
-
-                <div className="form_group">
-                    <label htmlFor="impact">Impact</label>
-                    <textarea id="impact" name="impact"></textarea>
-                </div>
-
-                <div className="form_group">
-                    <label htmlFor="prototype">Prototype</label>
-                    <textarea id="prototype" name="prototype"></textarea>
-                </div>
-
-            </div>
-
-            <div className='PostIdea_submit'>
-                <button type="submit">Submit Idea</button>
             </div>
         </div>
     );
