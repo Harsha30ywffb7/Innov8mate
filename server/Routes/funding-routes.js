7851458ebb7de75ad37dd88e531d91d4d0e23944
Harshn Vardhan Gulla <hvgulla@phizzle.com>
2024-12-13 15:29:39 +0530

    funding routes changed

--- server/Routes/funding-routes.js
+++ server/Routes/funding-routes.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import addCompany from '../controllers/funding/addcompany.js'
 import getCompanies from '../controllers/funding/companies.js';
+import updateCompanies from '../controllers/funding/update.js';
 
 const router = express.Router();
 
 router.post('/addcompany',addCompany);
 
 router.get('/', getCompanies);
+
+router.put('/update', updateCompanies);
 
 export default router;
